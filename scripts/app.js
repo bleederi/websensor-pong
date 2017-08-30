8cce0cd7a3f380efdcd020329531f4975f5d9a09
Jesse Nieminen <jesse.nieminen@intel.com>
2017-08-30 15:22:49 +0300

    Revert

--- scripts/app.js
+++ scripts/app.js
@@ -22,101 +22,48 @@
 
 'use strict';
 
-// If generic sensors are enabled and RelativeOrientationSensor is defined, create class normally
-// Otherwise create a fake class
-if('RelativeOrientationSensor' in window) {
-
-    // This is an inclination sensor that uses RelativeOrientationSensor
-    // and converts the quaternion to Euler angles, returning the longitude and latitude
-    window.RelativeInclinationSensor = class RelativeInclinationSensor extends RelativeOrientationSensor {
-        constructor(options) {
-            super(options);
-            this.longitude_ = 0;
-            this.latitude_ = 0;
-            this.longitudeInitial_ = 0;
-            this.initialOriObtained_ = false;
-        }
-
-        set onreading(func) {
-            super.onreading = () => {
-
-                // Conversion to Euler angles done in THREE.js so we have to create a
-                // THREE.js object for holding the quaternion to convert from
-                // Order x,y,z,w
-                let quaternion = new THREE.Quaternion(super.quaternion[0], super.quaternion[1],
-                                                      super.quaternion[2], super.quaternion[3]);
-
-                // euler will hold the Euler angles corresponding to the quaternion
-                let euler = new THREE.Euler(0, 0, 0);
-
-                // Order of rotations must be adapted depending on orientation
-                // for portrait ZYX, for landscape ZXY
+//This is an inclination sensor that uses RelativeOrientationSensor and converts the quaternion to Euler angles
+class RelativeInclinationSensor {
+        constructor() {
+        this.sensor_ = new RelativeOrientationSensor({ frequency: 60 });
+        this.x_ = 0;
+        this.y_ = 0;
+        this.z_ = 0;
+        this.sensor_.onreading = () => {
+                let quat = this.sensor_.quaternion;
+                let quaternion = new THREE.Quaternion();        //Conversion to Euler angles done in THREE.js so we have to create a THREE.js object for holding the quaternion to convert from
+                let euler = new THREE.Euler( 0, 0, 0);  //Will hold the Euler angles corresponding to the quaternion
+                quaternion.set(quat[0], quat[1], quat[2], quat[3]);     //x,y,z,w
+                //Order of rotations must be adapted depending on orientation - for portrait ZYX, for landscape ZXY
                 let angleOrder = null;
                 screen.orientation.angle === 0 ? angleOrder = 'ZYX' : angleOrder = 'ZXY';
-                euler.setFromQuaternion(quaternion, angleOrder);
-                if (!this.initialOriObtained_) {
-
-                    // Initial longitude needed to make the initial camera orientation
-                    // the same every time
-                    this.longitudeInitial_ = -euler.z;
-                    if (screen.orientation.angle === 90) {
-                        this.longitudeInitial_ = this.longitudeInitial_ + Math.PI/2;
-                    }
-                    this.initialOriObtained_ = true;
-                }
-
-                // Device orientation changes need to be taken into account
-                // when reading the sensor values by adding offsets
-                // Also the axis of rotation might change
-                switch (screen.orientation.angle) {
-
-                    // In case there are other screen orientation angle values,
-                    // for example 180 degrees (not implemented in Chrome), default is used
-                    default:    
-                    case 0:
-                        this.longitude_ = -euler.z - this.longitudeInitial_;
-                        this.latitude_ = euler.x - Math.PI/2;
-                        break; 
-                    case 90:
-                        this.longitude_ = -euler.z - this.longitudeInitial_ + Math.PI/2;
-                        this.latitude_ = -euler.y - Math.PI/2;                 
-                        break;     
-                    case 270:
-                        this.longitude_ = -euler.z - this.longitudeInitial_ - Math.PI/2;
-                        this.latitude_ = euler.y - Math.PI/2;
-                        break;
-                }
-                func();
-            };      
+                euler.setFromQuaternion(quaternion, angleOrder);     //ZYX works in portrait, ZXY in landscape
+                this.x_ = euler.x;
+                this.y_ = euler.y;
+                this.z_ = euler.z;
+                if (this.onreading_) this.onreading_();
+        };
         }
-
-        get longitude() {
-            return this.longitude_;
+        start() { this.sensor_.start(); }
+        stop() { this.sensor_.stop(); }
+        get x() {
+                return this.x_;
         }
-
-        get latitude() {
-            return this.latitude_;
+        get y() {
+                return this.y_;
+        } 
+        get z() {
+                return this.z_;
         }
-    }
-} else {
-    // Fake interface
-    window.RelativeInclinationSensor = class RelativeInclinationSensor {
-        constructor(options) {
-            this.start = function() {};
+        set onactivate(func) {
+                this.sensor_.onactivate_ = func;
         }
-
-        set onreading(func) {}
-
-        get longitude() {
-            return 0;
+        set onerror(err) {
+                this.sensor_.onerror_ = err;
         }
-
-        get latitude() {
-            return 0;
+        set onreading (func) {
+                this.onreading_ = func;  
         }
-    }
-    // Inform the user that generic sensors are not enabled
-    document.getElementById("no-sensors").style.display = "block";
 }
 
 //This is a shake detection sensor that uses Accelerometer
@@ -549,16 +496,16 @@ function playerPaddleMovement() {
         switch(screen.orientation.angle) {
                 default:
                 case 0:
-                        oriSensor.latitude < 0 ? direction = "left" : direction = "right";
-                        force = Math.abs(oriSensor.latitude);
+                        oriSensor.y < 0 ? direction = "left" : direction = "right";
+                        force = Math.abs(oriSensor.y);
                 break;
                 case 90:
-                        oriSensor.longitude < 0 ? direction = "left" : direction = "right";
-                        force = Math.abs(oriSensor.longitude);
+                        oriSensor.x < 0 ? direction = "left" : direction = "right";
+                        force = Math.abs(oriSensor.x);
                 break;
                 case 270:
-                        oriSensor.longitude < 0 ? direction = "right" : direction = "left";
-                        force = Math.abs(oriSensor.longitude);
+                        oriSensor.x < 0 ? direction = "right" : direction = "left";
+                        force = Math.abs(oriSensor.x);
                 break;
                 }
 	if (direction === "left")		
@@ -716,4 +663,5 @@ function checkRestart() {
         updateScoreboard("Bold 20px Arial","First to " + maxScore + " wins");
     }
 }
+
 
